cc93651b265f2aa891130f3326fb66c4e9bbd84c
hopezh <hope.zh@gmail.com>
2023-07-22 18:18:48 +0800

    install nvim-colorizer

---
+++ lua/plugins/colors.lua
@@ -0,0 +1,19 @@
+return {
+	-- color html colors
+	{
+		"norcalli/nvim-colorizer.lua",
+
+		config = function()
+			require("colorizer").setup({
+				css = { css_fn = true, css = true },
+				"javascript",
+				"html",
+				"r",
+				"rmd",
+				"qmd",
+				"markdown",
+				"python",
+			})
+		end,
+	},
+}
